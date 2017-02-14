6168623f2a44bbfbcdd677f4a14cef1d95b78ccf
SheilaOM <sheila.oliva14@gmail.com>
2017-02-14 16:41:40 +0100

    Sumador Simple terminado

--- servidor-sumador.py
+++ servidor-sumador.py
@@ -25,46 +25,63 @@ mySocket.listen(5)
 # Accept connections, read incoming data, and answer back an HTML page
 #  (in an almost-infinite loop; the loop can be stopped with Ctrl+C)
 
+cont = 0
+sumando = []
 try:
     while True:
         print ('Waiting for connections')
-        (recvSocket, address) = mySocket.accept()       #Me quedo con el objeto socket y una lista de 2 elementos: IP y puerto
+        (recvSocket, address) = mySocket.accept()
         print ('Request received:')
         peticion = recvSocket.recv(2048).decode("utf-8", "strict")
-        print (peticion)   #bytes -> utf-8
+        print (peticion)
         print ('Answering back...')
 
-        recurso = peticion.split()[1][1:] #por espacios y saltos de linea
+        recurso = peticion.split()[1][1:]
         if recurso == "favicon.ico":
-            recvSocket.send(bytes("HTTP/1.1 404 Not found\r\n\r\n<html><body><h1>Not Found</h1></body></html>", "utf-8"))
+            recvSocket.send(bytes("HTTP/1.1 404 Not found\r\n\r\n<html>" +
+                                  "<body><h1>Not Found</h1></body>" +
+                                  "</html>",
+                                  "utf-8"))
             recvSocket.close()
             continue
 
         try:
             recurso = int(recurso)
+            sumando.insert(cont, recurso)
+            print("Sumando " + str(cont) + ": " + str(sumando[cont]))
+            cont += 1
         except ValueError:
             recvSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n" +
-                            "<html><body><h1>Calculadora</h1>" +
-                            "<p>Me has dado un " + recurso + ". Vete" +
-                            "</p>" +
-                            "</body></html>" +
-                            "\r\n", "utf-8"))
+                                  "<html><body><h1>Calculadora</h1>" +
+                                  "<p>Me has dado un " + recurso + ". Vete" +
+                                  "</p>" +
+                                  "</body></html>" +
+                                  "\r\n", "utf-8"))
             recvSocket.close()
             continue
 
-        recvSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n" +
-                        "<html><body><h1>Calculadora</h1>" +
-                        "<p>Me has dado un " + str(recurso) + ". Dame mas" +
-                        "</p>" +
-                        "</body></html>" +
-                        "\r\n", "utf-8"))
-        recvSocket.close()
-except KeyboardInterrupt:       #^C
+        if cont == 1:
+            recvSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n" +
+                                  "<html><body><h1>Calculadora</h1>" +
+                                  "<p>Me has dado un " + str(sumando[cont-1]) +
+                                  ". Dame otro</p>" +
+                                  "</body></html>" +
+                                  "\r\n", "utf-8"))
+            recvSocket.close()
+        elif cont == 2:
+            suma = sumando[0]+sumando[1]
+            recvSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n" +
+                                  "<html><body><h1>Calculadora</h1>" +
+                                  "<p>Me habias dado un " +
+                                  str(sumando[cont-2]) +
+                                  ". Ahora me has dado un " +
+                                  str(sumando[cont-1]) +
+                                  ". La suma es " + str(suma) + "</p>" +
+                                  "</body></html>" +
+                                  "\r\n", "utf-8"))
+            recvSocket.close()
+            cont = 0
+
+except KeyboardInterrupt:
     print ("Closing binded socket")
     mySocket.close()
-
-
-#5 -> me has dado un 5. Dame más
-#a -> me has dado un a. Vete
-#3 -> me habias dado un 5. Ahora me has dado un 3. La suma es 8
-#4.5 -> me has dado un 4.5. Vete
